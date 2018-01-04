2432b7fabe654c092353dbda9c29331af2edf719
Gijs Middelkamp <g.middelkamp@previder.nl>
2018-01-04 12:49:41 +0100

    Initial commit

---
+++ Previder-Powershell-Module.psm1
@@ -0,0 +1,535 @@
+#
+# Module 'Previder-Powershell-Module'
+#
+
+$Annexus = @{
+  BaseUri = "https://portal.previder.nl";
+  Uri = "https://portal.previder.nl/api";
+  Headers = @{};
+}
+
+Add-Type -TypeDefinition @"
+  public enum VirtualMachineControlAction {
+    POWERON, RESTART, RESET, POWEROFF, SHUTDOWN, SUSPEND, INSTALL_GUEST_TOOLS,
+    UPGRADE_GUEST_TOOLS, POWERON_BIOS, RESTART_BIOS, RESET_BIOS, REFRESH_STATE
+  }
+"@
+
+Add-Type -TypeDefinition @"
+  public enum ProvisioningType {
+    NONE, CLOUD_INIT_OVF, SYSPREP, SYSPREP_TEXT
+  }
+"@
+
+function Connect-Annexus {
+  [CmdletBinding(DefaultParameterSetName="credSet")]
+  param(
+    [parameter(ParameterSetName="tokenSet", Mandatory=$TRUE)]
+    [string] $Token,
+    [parameter(ParameterSetName="credSet", Mandatory=$TRUE)]
+    [string] $Accountname,
+    [parameter(ParameterSetName="credSet", Mandatory=$TRUE)]
+    [string] $Username,
+    [parameter(ParameterSetName="credSet", Mandatory=$TRUE)]
+    [Security.SecureString] $Password,
+    [parameter(ParameterSetName="credSet", Mandatory=$FALSE)]
+    [string] $OTP,
+    [switch] $UseOTP,
+    [string] $Uri
+  )
+
+  If ($UseOTP -and !$OTP) {
+    $OTP = Read-Host 'OTP'
+  }
+  
+  If ($Uri) {
+    $Annexus.Uri = $Uri
+  }
+  
+  If ($Token) {
+    $Annexus.Headers.Add("X-Auth-Token", $Token)
+  } Else {
+
+    $UnmanagedString = [System.IntPtr]::Zero
+    try {
+      $UnmanagedString = [Runtime.InteropServices.Marshal]::SecureStringToGlobalAllocUnicode($Password);
+      $UnsecurePassword = [Runtime.InteropServices.Marshal]::PtrToStringUni($UnmanagedString);
+    } finally {
+      [Runtime.InteropServices.Marshal]::ZeroFreeGlobalAllocUnicode($UnmanagedString);
+    }
+        
+    $Credentials = @{
+      "username"="$($Username)@$($Accountname)";
+      "password"=$UnsecurePassword;
+      "otp"=$OTP;
+    }
+
+    $Res = Invoke-WebRequest -SessionVariable AnnexusSession -Method GET -Uri "$($Annexus.Uri)/"
+	
+    $Annexus.Session = $AnnexusSession
+    $Annexus.Session.Headers.Add("X-CSRF-Token", $Res.Headers["X-CSRF-Token"])
+	
+	
+    try {
+      $Res = Invoke-WebRequest -WebSession $Annexus.Session -Method POST -Uri "$($Annexus.Uri)/logincheck" -Body $Credentials
+    } catch {
+      Write-Host "Error logging in"
+      return
+    }
+	
+	# Newly required block of code since release of 19/12/2017
+    $Res = Invoke-WebRequest -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/session"
+	$Annexus.Session.Headers.Set_Item("X-CSRF-Token", $Res.Headers["X-CSRF-Token"])
+	# End of newly required block of code since release of 19/12/2017
+	
+    Write-Host "Logged in successfully"
+    $Res
+  }
+}
+
+function Disconnect-Annexus {
+  [CmdletBinding()]
+  param()
+
+  $Res = Invoke-WebRequest -WebSession $Annexus.Session -Method POST -Uri "$($Annexus.BaseUri)/logout"
+
+  $Annexus.Headers.Remove("X-Auth-Token")
+  if($Annexus.Session.Headers.ContainsKey('X-Auth-Token')){
+     $Annexus.Session.Headers.Remove("X-Auth-Token")
+  }
+  Write-Host "Logged out"
+}
+
+function Set-HandleByCustomer {
+  [CmdletBinding()]
+  param(
+    [parameter(Mandatory=$FALSE)]
+    [string] $Id
+  )
+  
+  if ($Id) {
+    $Customer = Get-Customer -Id $Id
+    Write-Host "Handle by customer $($Customer.name)"
+    $Annexus.Headers.Add("X-CustomerId", $Customer.id)
+  } else {
+    Write-Host "Clear handle by customer"
+    $Annexus.Headers.Remove("X-CustomerId")
+    $Annexus.Session.Headers.Remove("X-CustomerId")
+  }
+}
+
+function Get-CustomerList {
+  [CmdletBinding()]
+  param()
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/customer"
+  $Res
+}
+
+function Get-Customer {
+  [CmdletBinding()]
+  param(
+    [parameter(Mandatory=$TRUE)]
+    [string] $Id
+  )
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/customer/$($Id)"
+  $Res
+}
+
+
+function Get-VmList {
+  [CmdletBinding()]
+  param()
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/virtualmachine"
+  $Res
+}
+
+function Get-VmNetworkList {
+  [CmdletBinding()]
+  param()
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/virtualnetwork"
+  $Res
+}
+
+function Get-VmClusterList {
+  [CmdletBinding()]
+  param()
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/virtualmachine/cluster"
+  $Res
+}
+
+function Get-VmTemplateList {
+  [CmdletBinding()]
+  param()
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/virtualmachine/template"
+  $Res
+}
+
+function Get-VmGroupList {
+  [CmdletBinding()]
+  param()
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/configurationitem/group"
+  $Res
+}
+
+function Get-Vm {
+  [CmdletBinding(DefaultParameterSetName="nameSet")]
+  param(
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [parameter(ParameterSetName="nameSet", Mandatory=$TRUE)]
+    [string] $Name,
+    [parameter(ParameterSetName="vmIdSet", Mandatory=$TRUE)]
+    [string] $VmId
+  )
+
+  If ($Id) {
+    $Uri = "$($Annexus.Uri)/virtualmachine/$($Id)"
+  } ElseIf ($Name) {
+    $Uri = "$($Annexus.Uri)/virtualmachine/byname/$($Name)"
+  } ElseIf ($VmId) {
+    $Uri = "$($Annexus.Uri)/virtualmachine/byvmid/$($VmId)"
+  }
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri $Uri
+  $Res
+}
+
+function Remove-Vm {
+  [CmdletBinding(DefaultParameterSetName="nameSet")]
+  param(
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [parameter(ParameterSetName="nameSet", Mandatory=$TRUE)]
+    [string] $Name
+  )
+  
+  if ($Name) {
+    $Vm = Get-Vm -Name $Name
+    $Id = $Vm.id
+  }
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method DELETE -Uri "$($Annexus.Uri)/virtualmachine/$($Id)"
+  $Res
+}
+
+function Set-Vm {
+  [CmdletBinding()]
+  param(
+    [parameter(Mandatory=$TRUE)]
+    [string] $Id,
+    [string] $Name,
+    [string] $Group,
+    [int] $CpuCores,
+    [int] $MemoryMb,
+    [boolean] $TerminationProtection,
+    [string[]] $Tags
+  )
+  
+  $Vm = Get-Vm -Id $Id
+  
+  If ($Name) {
+    $Vm.name = $Name
+  }
+
+  If ($CpuCores) {
+    $Vm.cpuCores = $CpuCores
+  }
+  
+  If ($MemoryMb) {
+    $Vm.memoryMb = $MemoryMb
+  }
+  
+  if ($Tags) {
+	$Vm.tags = $Tags
+  }
+  
+  If ($TerminationProtection) {
+    $Vm.terminationProtection = $TerminationProtection
+  }
+  
+  if ($Group) {
+    $groupObj = VmGroupList | Where-Object {$_.name -eq $Group}
+    if (!$groupObj) {
+      Throw "group not found: " + $Group
+    }
+    $Vm.group = $groupObj
+  }
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method PUT -Uri "$($Annexus.Uri)/virtualmachine/$($Id)" -Body ($Vm | ConvertTo-Json -Depth 10)
+  $Res
+}
+
+function New-Vm {
+  [CmdletBinding(DefaultParameterSetName="newSet")]
+  param(
+    [parameter(Mandatory=$TRUE)]
+    [string] $Name,
+    [string] $Group,
+    [string] $Cluster = "Express",
+    [parameter(ParameterSetName="newSet", Mandatory=$TRUE)]
+    [string] $Template,
+    [parameter(ParameterSetName="copySet", Mandatory=$TRUE)]
+    [string] $SourceVmId,
+    [int] $CpuCores = 1,
+    [int] $MemoryMb = 1024,
+    [ProvisioningType] $ProvisioningType,
+    [string] $UserData,
+    [parameter(Mandatory=$TRUE)]
+    [string[]] $Nics,
+    [parameter(Mandatory=$TRUE)]
+    [int[]] $Disks,
+    [string[]] $Tags
+  )
+  
+  if ($Group) {
+    $groupObj = Get-VmGroupList | Where-Object {$_.name -eq $Group}
+    if (!$groupObj) {
+      Throw "group not found: " + $Group
+    }
+  }
+
+  If ($Template) {
+    $templateObj = Get-VmTemplateList | Where-Object {$_.name -eq $Template}
+    if (!$templateObj) {
+      Throw "template not found: " + $Template
+    }
+  } ElseIf ($SourceVmId) {
+    $sourceVmObj = Get-Vm -Id $SourceVmId
+    if (!$sourceVmObj) {
+      Throw "source vm not found: " + $SourceVmId
+    }
+  }
+
+  $computeClusterObj = Get-VmClusterList | Where-Object {$_.name -eq $Cluster}
+  if (!$computeClusterObj) {
+    Throw "cluster not found: " + $Cluster
+  }
+  
+  $virtualDisks = [System.Collections.ArrayList]@()
+  ForEach ($diskSize in $Disks) {
+    [void]$virtualDisks.Add(@{
+      "diskSize"=$diskSize;
+    })
+  }
+
+  $networkInterfaces = [System.Collections.ArrayList]@()
+  ForEach ($networkName in $Nics) {
+    $network = Get-VmNetworkList | Where-Object {$_.name -eq $networkName}
+    if (!$network) {
+      Throw "network not found: " + $networkName
+    }
+    [void]$networkInterfaces.Add(@{
+      "network"=@{
+        "id"=$network.id
+      };
+      "connected"=$TRUE;
+    })
+  }
+  
+  $vm = @{
+    "name"=$Name;
+    "cpuCores"=$CpuCores;
+    "memoryMb"=$MemoryMb;
+    "networkInterfaces"=$networkInterfaces;
+    "virtualDisks"=$virtualDisks;
+    "userData"=$UserData;
+    "group"=$groupObj;
+    "template"=$templateObj;
+    "computeCluster"=$computeClusterObj;
+    "provisioningType"=$ProvisioningType;
+	"tags"=$Tags
+  }
+  
+  if ($sourceVmObj) {
+    $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method POST -Uri "$($Annexus.Uri)/virtualmachine/$($SourceVmId)/clone" -Body ($Vm | ConvertTo-Json -Depth 10) -ContentType "application/json"
+  } else {
+    $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method POST -Uri "$($Annexus.Uri)/virtualmachine" -Body ($Vm | ConvertTo-Json -Depth 10) -ContentType "application/json"
+  }
+  $Res
+}
+
+function Invoke-Vm {
+  [CmdletBinding(DefaultParameterSetName="nameSet")]
+  param(
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [parameter(ParameterSetName="nameSet", Mandatory=$TRUE)]
+    [string] $Name,
+    [parameter(Mandatory=$TRUE)]
+    [VirtualMachineControlAction] $Action
+  )
+  
+  if ($Name) {
+    $Vm = Get-Vm -Name $Name
+    $Id = $Vm.id
+  }
+ 
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method POST -Uri "$($Annexus.Uri)/virtualmachine/$($Id)/control/$($Action)" -ContentType "application/json"
+  $Res
+}
+
+function Invoke-VmConsole {
+  [CmdletBinding(DefaultParameterSetName="nameSet")]
+  param(
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [parameter(ParameterSetName="nameSet", Mandatory=$TRUE)]
+    [string] $Name
+  )
+  
+  if ($Name) {
+    $Vm = Get-Vm -Name $Name
+    $Id = $Vm.id
+  }
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method POST -Uri "$($Annexus.Uri)/virtualmachine/$($Id)/console" -ContentType "application/json"
+  Start-Process $Res.result
+  $Res
+}
+
+
+function New-VmSnapshot {
+  [CmdletBinding(DefaultParameterSetName="nameSet")]
+  param(
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [parameter(ParameterSetName="nameSet", Mandatory=$TRUE)]
+    [string] $Name,
+    [parameter(Mandatory=$TRUE)]
+    [string] $Decription,
+    [switch] $Memory,
+    [switch] $Quiesce
+  )
+  
+  if ($Name) {
+    $Vm = Get-Vm -Name $Name
+    $Id = $Vm.id
+  }
+  
+  $Snapshot = @{
+    "name"=$Description
+    "memory"=$Memory;
+    "quiesce"=$Quiesce;
+  }
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method POST -Uri "$($Annexus.Uri)/virtualmachine/$($Id)/snapshot" -Body ($Snapshot | ConvertTo-Json) -ContentType "application/json"
+  $Res
+}
+
+function Remove-VmSnapshot {
+  [CmdletBinding(DefaultParameterSetName="nameSet")]
+  param(
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [parameter(ParameterSetName="nameSet", Mandatory=$TRUE)]
+    [string] $Name,
+    [parameter(Mandatory=$TRUE)]
+    [int] $SnapshotId,
+    [switch] $RemoveChildren
+  )
+
+  if ($Name) {
+    $Vm = Get-Vm -Name $Name
+    $Id = $Vm.id
+  }
+  
+  $Snapshot = @{
+    "removeChildren"=$RemoveChildren;
+  }
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method PUT -Uri "$($Annexus.Uri)/virtualmachine/$($Id)/snapshot/$($SnapshotId)/remove" -Body ($Snapshot | ConvertTo-Json)
+  $Res
+}
+
+function Reset-VmSnapshot {
+  [CmdletBinding(DefaultParameterSetName="nameSet")]
+  param(
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [parameter(ParameterSetName="nameSet", Mandatory=$TRUE)]
+    [string] $Name,
+    [parameter(Mandatory=$TRUE)]
+    [int] $SnapshotId
+  )
+  
+  if ($Name) {
+    $Vm = Get-Vm -Name $Name
+    $Id = $Vm.id
+  }
+  
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method PUT -Uri "$($Annexus.Uri)/virtualmachine/$($Id)/snapshot/$($SnapshotId)/revert"
+  $Res
+}
+
+function Get-VmTask {
+  [CmdletBinding()]
+  param(
+    [parameter(Mandatory=$FALSE)]
+    [string] $Id
+  )
+
+  $Res = Invoke-RestMethod -WebSession $Annexus.Session -Headers $Annexus.Headers -Method GET -Uri "$($Annexus.Uri)/configurationitem/task/$($Id)"
+  $Res
+}
+
+function Wait-VmDeploy {
+  [CmdletBinding(DefaultParameterSetName="nameSet")]
+  param(
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [parameter(ParameterSetName="nameSet", Mandatory=$TRUE)]
+    [string] $Name,
+    [int] $Timeout = 3600
+  )
+  
+  if ($Name) {
+    $Vm = Get-Vm -Name $Name
+    $Id = $Vm.id
+  }
+  
+  $Timespan = New-TimeSpan -Seconds $Timeout
+  $State = ""
+   
+  $Sw = [Diagnostics.Stopwatch]::StartNew()
+  while (!$State -eq "POWEREDON" -and $Sw.Elapsed -lt $Timespan) {
+    $Vm = Get-Vm -Id $Id
+    $State = $Vm.state
+    Start-Sleep -Seconds 3
+  }
+  
+  $Vm
+}
+
+function Wait-VmTask {
+  [CmdletBinding(DefaultParameterSetName="taskSet")]
+  param(
+    [parameter(ParameterSetName="taskSet", Mandatory=$TRUE, ValueFromPipeline=$TRUE)]
+    $Task,
+    [parameter(ParameterSetName="idSet", Mandatory=$TRUE)]
+    [string] $Id,
+    [int] $Timeout = 60
+  )
+  
+  $Timespan = New-TimeSpan -Seconds $Timeout
+  $Completed = $FALSE
+  
+  if ($Task){
+    $Id = $Task.id
+  }
+  
+  $Sw = [Diagnostics.Stopwatch]::StartNew()
+  while (!$Completed -and $Sw.Elapsed -lt $Timespan) {
+    $Task = Get-VmTask -Id $Id
+    $Completed = $Task.completed
+    Start-Sleep -Seconds 1
+  }
+
+  $Task
+}
---
+++ README.md
@@ -0,0 +1,113 @@
+# Previder Powershell Module
+The Previder Powershell Module is used to interact with resources on the Previder IaaS environment. 
+The provider needs to be configured with an API token that will be provided by Previder.
+
+## Example Usage
+
+```
+Import-Module Previder-Powershell-Module
+Connect-Annexus -Token <token>
+Get-VmList
+New-Vm -Name "Virtual Server 01" -Cluster "Express" -Template "Ubuntu 17.10" -CpuCores 2 -MemoryMb 2048 -Nics ["Public WAN"] -Disks [20480]
+Wait-VmDeploy -Name "Virtual Server 01"
+Invoke-VmConsole -Name "Virtual Server 01"
+```
+
+## Argument reference
+
+The following arguments are supported
+
+### Connect-Annexus
+
+*(Either Token or Accountname and Username are required)*
+- Token -  This is your personal API token for accessing resources in the Previder IaaS environment.
+- Accountname - This is your accountname
+- Username - Enter the username with which to login
+- Password - Enter the plain-text password required for the account configure above
+- OTP - If OTP is enabled on this account, use this field to send the challenge code
+- UseOTP - Required when manually sending your OTP challenge code
+- Uri - Only used when connecting to an alternative environment (defaults to https://portal.previder.nl/api)
+
+### Set-HandleByCustomer
+- Id (optional) - Id of the subcustomer which you loke to switch to (to clear this setting, only call base function)
+
+### Get-Customer
+- Id (required) - Id of the customer which you like to show
+
+### Get-Vm
+- Id (optional) - Id of the Virtual Server you would like to show
+- Name (Optional) - Name of the Virtual Server you would like to show
+- VmId (optional) - Backend Id of the Virtual Server you would like to show
+
+### Remove-Vm
+- Id (optional) - Id of the Virtual Server you would like to remove
+- Name (optional) - Name of the Virtual Server you would like to remove
+
+### Set-Vm
+- Id (required) - Id of the Virtual Server you would like to edit
+- Name (optional) - Name to set the Virtual Server to
+- Group (optional) - Name of the group to set the Virtual Server to
+- CpuCores (optional) - Integer of number of cpu cores
+- MemoryMb (optional) - Integer of MBs of memory to set the VM to
+- TerminationProtection (optional) - Boolean of the termination protection
+- Tags (optional) - List of strings to set specifuc tags on the Virtual Server
+
+### New-Vm
+- Name (required) - Name to set the Virtual Server to
+- Group (optional) - Name of the group to set the Virtual Server to
+- Cluster (required) - Name of the compute cluster to deploy the Virtual Server to
+- Template (optional) - Name of the template to deploy (Either Template or SourceVmId is required)
+- SourceVmId (optional) - Id of the Virtual Server to clone (Either Template or SourceVmId is required)
+- CpuCores (required) - Integer of number of cpu cores
+- MemoryMb (required) - Integer of MBs of memory to set the VM to
+- ProvisioningType (optional) - Provide your own provisioningtype, leave blank to copy it of the template
+- UserData (optional) - Send custom userdata (leave blank if you do not know what to enter)
+- Nics (required) - List of network names to link the Virtual Server to
+- Disks (required) - List of integers for disk sizes
+- Tags (optional) - List of strings to set specifuc tags on the Virtual Server
+
+### Invoke-Vm
+- Id (optional) - Id of the Virtual Server to send the action to (Either Id or Name is required)
+- Name (optional) - Name of the Virtual Server to send the action to (Either Id or Name is required)
+- Action (required) - Action to send
+
+### Invoke-VmConsole
+- Id (optional) - Id of the Virtual Server to open the console of (Either Id or Name is required)
+- Name (optional) - Name of the Virtual Server to open the console of (Either Id or Name is required)
+
+### New-VmSnapshot
+- Id (optional) - Id of the Virtual Server to create a snapshot of (Either Id or Name is required)
+- Name (optional) - Name of the Virtual Server to create a snapshot of (Either Id or Name is required)
+- Description (required) - Description for this snapshot
+
+### Remove-VmSnapshot
+- Id (optional) - Id of the Virtual Server to remove a snapshot of (Either Id or Name is required)
+- Name (optional) - Name of the Virtual Server to remove a snapshot of (Either Id or Name is required)
+- SnapshotId (required) - Integer snapshot id
+- RemoveChildren (optional) - Boolean if you wish to remove all snapshots embedded in the snapshot to remove
+
+### Reset-VmSnapshot
+- Id (optional) - Id of the Virtual Server to revert a snapshot of (Either Id or Name is required)
+- Name (optional) - Name of the Virtual Server to revert a snapshot of (Either Id or Name is required)
+- SnapshotId (required) - Integer snapshot id
+
+### Get-VmTask
+- Id (required) - Id of the task you wish to show
+
+### Wait-VmDeploy
+- Id (optional) - Id of the Virtual Server to wait until deployed (Either Id or Name is required)
+- Name (optional) - Name of the Virtual Server to wait until deployed (Either Id or Name is required)
+- Timeout (optional) - Integer in seconds for timeout (defaults to 3600 or 1 hour)
+
+
+## Motivation
+
+As projects besides e.g. the Previder Portal, the development team at Previder develops and maintains multiple projects aiming to integrate the previder IaaS environment.
+
+## Contributors
+
+* Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet
+* Fork the project
+* Start a feature/bugfix branch
+* Commit and push until you are happy with your contribution
+* Send a merge request describing your exact problem, what and how you fixed it
